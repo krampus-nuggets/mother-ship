36a472f415683dc1683792957c7eca442dcb1f03
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-20 14:26:27 +0200

    schema -> add types

--- mother-ship-backend/src/schema.js
+++ mother-ship-backend/src/schema.js
@@ -1,5 +1,17 @@
 const schema = `
 
+"""
+TYPE
+"""
+type Upload {
+    userId: String!,
+    itemId: String!,
+    title: String!,
+    fileSize: String!,
+    fileURL: String!,
+    dateUploaded: String!,
+    fileType: String!
+}
 
 
 """
