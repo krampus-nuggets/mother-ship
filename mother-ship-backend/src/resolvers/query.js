2b556d77d395342b35cac7a980aecbdf66ab4c7d
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-20 14:48:09 +0200

    Add -> getAllUploads query

--- mother-ship-backend/src/resolvers/query.js
+++ mother-ship-backend/src/resolvers/query.js
@@ -3,3 +3,26 @@ import *  as dynamoLib from "../../resources/libs/dynamodb-lib";
 export const hello = (args, context) => {
     return "Your GraphQL API is now LIVE!🎈 "
 }
+
+export const getAllUploads = async (args, context) => {
+    const params = {
+        TableName: process.env.MotherShipTable
+    }
+    try {
+        const result = await dynamoLib.call("scan", params);
+        result.Items.map((item) => (
+            {
+                userId: item.pk,
+                itemId: item.sk,
+                title: item.title,
+                fileSize: item.fileSize,
+                fileURL: item.fileURL,
+                dateUploaded: item.dateUploaded,
+                fileType: item.fileType
+            }
+        ))
+    }
+    catch (e) {
+        return e
+    }
+}
