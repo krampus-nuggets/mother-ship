791f8f620ecf19daf2af909d0a232776f72ffa3a
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-10 01:59:22 +0200

    Update -> add additional comp to cont

--- mother-ship-frontend/src/App.js
+++ mother-ship-frontend/src/App.js
@@ -9,13 +9,15 @@ const App = () => {
     <ThemeProvider>
       <ColorModeProvider>
         <div id="mother-ship-nav">
-          <Box display="flex" justifyContent="space-between" bg="#ff8b0f" m={ 0 } p={ 2 } color="white">
-            <Heading className="ms-nav-head" bg="black" color="#ff8b0f" rounded="md" p={ 2 } size="md" textAlign="center">
-              Mother-Ship
-            </Heading>
-          </Box>
+            <Box display="flex" justifyContent="space-between" bg="#ff8b0f" m={ 0 } p={ 2 } color="white">
+              <Heading className="ms-nav-head" bg="black" color="#ff8b0f" rounded="md" p={ 2 } size="md" textAlign="center">
+                Mother-Ship
+              </Heading>
+            </Box>
         </div>
-        <Home/>
+        <div id="ms-body-components">
+          <Home/>
+        </div>
       </ColorModeProvider>
     </ThemeProvider>
   )
