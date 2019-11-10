abcdbc0ab3e85c309ba34f1eef278d79edc5f5c6
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-10 17:41:54 +0200

    Update -> wrap header w/ link

--- mother-ship-frontend/src/App.js
+++ mother-ship-frontend/src/App.js
@@ -10,9 +10,11 @@ const App = () => {
       <ColorModeProvider>
         <div id="mother-ship-nav">
             <Box display="flex" justifyContent="space-between" bg="#ff8b0f" m={ 0 } p={ 2 } color="white">
+              <Link href="/" m="auto" isInternal>
               <Heading className="ms-nav-head" bg="black" color="#ff8b0f" rounded="md" p={ 2 } size="md" textAlign="center">
                 Mother-Ship
               </Heading>
+              </Link>
             </Box>
         </div>
         <div id="ms-body-components">
