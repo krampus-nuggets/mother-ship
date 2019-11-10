6d4306b15ad28eff5cb033b265e1eafeb76a64c5
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-10 18:14:32 +0200

    Update -> imports

--- mother-ship-frontend/src/Routes.js
+++ mother-ship-frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import SignUp from "./containers/SignUp";
+import Login from "./containers/Login";
 
 export default () => 
     <Switch>
