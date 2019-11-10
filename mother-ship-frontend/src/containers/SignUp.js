2ee91a46527c65845457601b0093d8606ffa09d8
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-10 18:15:00 +0200

    Update -> imports

--- mother-ship-frontend/src/containers/SignUp.js
+++ mother-ship-frontend/src/containers/SignUp.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "../styles/styles.css";
 import { FormHelperText, FormLabel, FormControl, Button, Input, Heading, Box } from "@chakra-ui/core";
+import "../styles/styles.css";
 
 //const SignUp = () => {
     // Addition Func - Later
