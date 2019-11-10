2d5ef9fb1b690acf2c15b886b9b53e853d6060e9
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-10 17:42:08 +0200

    Update -> add routing

--- mother-ship-frontend/src/App.js
+++ mother-ship-frontend/src/App.js
@@ -11,14 +11,14 @@ const App = () => {
         <div id="mother-ship-nav">
             <Box display="flex" justifyContent="space-between" bg="#ff8b0f" m={ 0 } p={ 2 } color="white">
               <Link href="/" m="auto" isInternal>
-              <Heading className="ms-nav-head" bg="black" color="#ff8b0f" rounded="md" p={ 2 } size="md" textAlign="center">
-                Mother-Ship
-              </Heading>
+                <Heading className="ms-nav-head" bg="black" color="#ff8b0f" rounded="md" p={ 2 } size="md" textAlign="center">
+                  Mother-Ship
+                </Heading>
               </Link>
             </Box>
         </div>
         <div id="ms-body-components">
-          <Home/>
+          <Routes />
         </div>
       </ColorModeProvider>
     </ThemeProvider>
