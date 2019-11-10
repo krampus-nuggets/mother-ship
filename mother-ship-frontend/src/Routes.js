25f8b93988655f823dafe0a53857948d523cabb9
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-10 17:42:43 +0200

    Add -> Home & SignUp routes

--- mother-ship-frontend/src/Routes.js
+++ mother-ship-frontend/src/Routes.js
@@ -3,3 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import SignUp from "./containers/SignUp";
 
+export default () => 
+    <Switch>
+        <Route path="/" exact component={ Home } />
+        <Route path="/SignUp" exact component={ SignUp } />
+    </Switch>
