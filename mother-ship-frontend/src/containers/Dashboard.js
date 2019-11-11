6afa92517c90e90e9ca3fb812460b44b12532e0b
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-11 16:04:12 +0200

    Add -> update dashboard container

--- mother-ship-frontend/src/containers/Dashboard.js
+++ mother-ship-frontend/src/containers/Dashboard.js
@@ -1,3 +1,31 @@
 import React from "react";
 import { Box, Button, Heading, Link } from "@chakra-ui/core";
 
+const Dashboard = () => {
+    return(
+        <div id="ms-dashboard">
+            <Heading
+                textAlign="center" 
+                bg="Black"
+                color="#ff8b0f"
+                ml="40%"
+                mr="40%"
+                p="8px"
+                borderWidth="3px"
+                borderRadius="10px"
+            >
+                Dashboard
+            </Heading>
+            <Box
+                bg="Black"
+                color="#ff8b0f"
+                m="auto"
+                width="1000px"
+                height="550px"
+                borderRadius="15px"
+            >
+                
+            </Box>
+        </div>
+    )
+}
