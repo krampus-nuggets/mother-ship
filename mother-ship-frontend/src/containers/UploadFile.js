ccfa3350b2e92d8e5f8c32eef439552d5daa3a22
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-15 21:04:10 +0200

    ADD -> baseline container

--- mother-ship-frontend/src/containers/UploadFile.js
+++ mother-ship-frontend/src/containers/UploadFile.js
@@ -1,3 +1,29 @@
 import React from "react";
 import { Box, Input, Heading, FormControl, FormLabel } from "@chakra-ui/core";
 
+const UploadFile = () => {
+    return(
+        <div id="ms-upload-file">
+            <Box
+                m="8% 35% 10% 35%"
+                display="block"
+                color="White"
+            >
+                <form size="md">
+                    <Heading>
+                        Upload File
+                    </Heading>
+                    <FormControl size="md">
+                        <FormLabel>Select Your File</FormLabel>
+                            <Input
+                                id="ms-file-input"
+                                type="file"
+                            />
+                    </FormControl>
+                </form>
+            </Box>
+        </div>
+    )
+}
+
+export default UploadFile
