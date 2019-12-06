4fb3e7df736aef0889106e101932374214cc2a56
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-12-06 15:06:12 +0200

    UPDATE -> imports

--- mother-ship-frontend/src/index.js
+++ mother-ship-frontend/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
 import { BrowserRouter as  Router } from "react-router-dom";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
 import "./styles/styles.css";
 import App from './App';
+import config from "./res/configs/config";
 import * as serviceWorker from './serviceWorker';
 
 Amplify.configure({
