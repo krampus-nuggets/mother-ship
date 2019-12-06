4be6b9d8c8e85a2adc31b724ebb177b36d1bbfc1
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-12-06 15:06:00 +0200

    UPDATE -> add amplify configuration

--- mother-ship-frontend/src/index.js
+++ mother-ship-frontend/src/index.js
@@ -5,6 +5,39 @@ import "./styles/styles.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: false,
+        region: config.apiGateway.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: "mother-ship",
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            }
+        ]
+    }
+});
+
+const httpLink = createHttpLink({
+    uri: "http://localhost:4000/graphql"
+});
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
 ReactDOM.render(
         <Router>
             <App />
